976a19b20af3c1f260e0cb79862b0cca6f2e2425
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2023-01-23 09:27:45 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,40 +1,30 @@
-#!/usr/bin/python
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-import hashlib
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """Represents a user for a MySQL database.
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
 
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, password):
-        """Hashes a user password with MD5"""
-        encryption = hashlib.md5(password.encode())
-        self._password = encryption.hexdigest()
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
